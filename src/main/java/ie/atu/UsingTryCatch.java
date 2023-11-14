a28de2b27af32c8634a193a8cb2867f52a8a9a1e
G00413711@atu.ie <G00413711@Atu.ie>
2023-11-14 13:36:39 +0000

    Added try catch to class

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -1,24 +1,29 @@
 package ie.atu;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0 - 9 :");
-        Scanner myscan = new Scanner(System.in);
+        try {
+            System.out.println("Please enter a number between 0 - 9 :");
+            Scanner myscan = new Scanner(System.in);
 
-        //Read user input as a string
-        String input = myscan.nextLine();
+            //Read user input as a string
+            String input = myscan.nextLine();
 
-        //Parse the input to an integer
-        int myNum = Integer.parseInt(input);
+            //Parse the input to an integer
+            int myNum = Integer.parseInt(input);
 
-        //Check if the number is between 0 and 9
-        if(myNum>=0&& myNum <=9){
-            System.out.println("You entered: "+ myNum);
-        }
-        else {
+            //Check if the number is between 0 and 9
+            if (myNum >= 0 && myNum <= 9) {
+                System.out.println("You entered: " + myNum);
+            } else {
+                System.out.println("Not a valid Number");
+            }
+        }catch(NumberFormatException e){
             System.out.println("Not a valid Number");
         }
+
     }
 }
